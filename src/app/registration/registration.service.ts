825a53d6a6534ce1707b8aed77735fe252acb73e
Antonina Putova <Antanina_Putava@epam.com>
2023-03-22 19:41:07 +0100

    Changes from main1

--- src/app/registration/registration.service.ts
+++ src/app/registration/registration.service.ts
@@ -57,7 +57,7 @@ export class RegistrationService {
           errorText: validatorConfig.message
         };
       default:
-        throw new Error("[Validator from main3]: Unknown type");
+        throw new Error("[Validator from main]: Unknown type");
     }
   }
 }
